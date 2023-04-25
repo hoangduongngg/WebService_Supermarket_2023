c16ddee179f465360621ce655545f339973c50b7
Hoang Duong Nguyen <hoangduongnguyen.tech@gmail.com>
2023-04-25 15:39:29 +0700

    update

--- OrderService/src/main/java/com/example/orderservice/controller/OrderController.java
+++ OrderService/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -1,4 +1,13 @@
 package com.example.orderservice.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class OrderController {
+    public String Order() {
+        return "hello";
+    }
 }
